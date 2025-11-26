b2b5b0e4a5d0f0f3fe3bb765c3b11964ab313bde
victor gomes <victorgomesdacosta18@gmail.com>
2025-11-25 23:58:04 -0300

    feat: create and implement data context

---
+++ src/SiggAgroCoop.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using SiggAgroCoop.Domain.Entities;
+
+namespace SiggAgroCoop.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Farm> Farms { get; set; } = default!;
+    public DbSet<Sector> Sectors { get; set; } = default!;
+    public DbSet<Field> Fields { get; set; } = default!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Example: relationships
+        modelBuilder.Entity<Sector>()
+            .HasOne(s => s.Farm)
+            .WithMany(f => f.Sectors)
+            .HasForeignKey(s => s.FarmId);
+
+        modelBuilder.Entity<Field>()
+            .HasOne(f => f.Sector)
+            .WithMany(s => s.Fields)
+            .HasForeignKey(f => f.SectorId);
+    }
+}
